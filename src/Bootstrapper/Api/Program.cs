1b07461daafc55c9f474580671e46240848ce0ca
magicalcomputerguy <ramizkaanbicakci@outlook.com>
2026-02-18 23:18:11 +0300

    chore: module references and dependency injection

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,5 +1,13 @@
+using Basket;
+using Catalog;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
